f1821a2739c0836eccf5a03639f98e91462e2628
John P Baltazar <john.baltazar03@gmail.com>
2018-09-29 01:05:52 -0700

    API's

---
+++ API/first_request.js
@@ -0,0 +1,6 @@
+var request = require('request');
+request('http://www.google.com', function(error, response, body){
+    console.log('error: ', error);
+    console.log('statusCode: ', response && response.statusCode);
+    console.log('body: ', body);
+});
